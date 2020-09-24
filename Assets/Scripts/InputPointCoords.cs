cd62b1e56c40940a0ff7d2231907ad53d21a9888
zack on unity <zack256@users.noreply.github.com>
2020-09-24 19:38:14 -0400

    basic line segment adding

--- Assets/Scripts/InputPointCoords.cs
+++ Assets/Scripts/InputPointCoords.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics;   // with this library gotta use UnityEngine.Debug 
+using System.Security.Cryptography;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -11,19 +12,89 @@ public class InputPointCoords : MonoBehaviour
     public GameObject pointPrefab;
     public Button createPointButton;
     public GameObject pointsParent;
-    //public Dictionary<string, GameObject> pointsDict;
+    public Button createLineSegmentButton;
+    public GameObject lineSegmentsParent;
+    public GameObject lineSegmentPrefab;
+    public Camera cameraObject;
 
-    private Dictionary<string, GameObject> pointsDict = new Dictionary<string, GameObject>();
-    
+    //private Dictionary<string, GameObject> pointsDict = new Dictionary<string, GameObject>();
+    private Dictionary<GameObject, string> pointsDict = new Dictionary<GameObject, string>();
+    private int createLineSegmentMode = 0;
+    private GameObject firstLineSegmentSegment;
+
+    void HandleMouse (bool mouseWasClicked)
+    {
+        Transform mouseHitTransform = GetMousePointerObject();
+        if (mouseHitTransform)
+        {
+            GameObject mouseHitObject = mouseHitTransform.gameObject;
+            if (pointsDict.ContainsKey(mouseHitObject))
+            {
+                if (createLineSegmentMode == 1)
+                {
+                    if (mouseWasClicked)
+                    {
+                        firstLineSegmentSegment = mouseHitObject;
+                        UnityEngine.Debug.Log("Select second point.");
+                        createLineSegmentMode++;
+                    }
+                } else if (createLineSegmentMode == 2)
+                {
+                    if (mouseWasClicked)
+                    {
+                        if (mouseHitObject == firstLineSegmentSegment)
+                        {
+                            UnityEngine.Debug.Log("Cannot have second segment as same point!");
+                            return;
+                        }
+                        Vector3[] lineSegmentPositons = new Vector3[] { firstLineSegmentSegment.transform.position, mouseHitObject.transform.position };
+                        GameObject lineSegmentObject = Instantiate(lineSegmentPrefab, new Vector3(0, 0, 0), Quaternion.identity);
+                        lineSegmentObject.GetComponent<LineRenderer>().SetPositions(lineSegmentPositons);
+                        lineSegmentObject.transform.parent = lineSegmentsParent.transform;
+                        createLineSegmentMode = 0;
+                    }
+                }
+            }
+        } else
+        {
+
+        }
+    }
+
+    Transform GetMousePointerObject()
+    {
+        RaycastHit hit;
+        Ray ray = cameraObject.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(ray, out hit))
+        {
+            Transform objectHit = hit.transform;
+            return objectHit;
+        }
+        return null;
+    }
+
     bool isValidPointName (string wantedName)
     {
         return wantedName.Length > 0 && char.IsLetter(wantedName[0]);
     }
 
+    bool PointNameTaken (string query)
+    {
+        foreach (var pointName in pointsDict.Values)
+        {
+            if (query == pointName)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
     bool ParseAndInstantiatePoint ()
     {
         string wantedName = pointNameInputField.text;
-        if ((wantedName == "") || (pointsDict.ContainsKey(wantedName)) || (!isValidPointName(wantedName)))
+        if ((wantedName == "") || (!isValidPointName(wantedName)) || (PointNameTaken(wantedName)))
         {
             UnityEngine.Debug.Log("bad point name!!");
             return false;
@@ -60,7 +131,8 @@ public class InputPointCoords : MonoBehaviour
         }
         Vector3 pointCoords = new Vector3((float) xCoord, (float) yCoord, (float) zCoord);
         GameObject point = Instantiate(pointPrefab, pointCoords, Quaternion.identity);
-        pointsDict[wantedName] = point;
+        //pointsDict[wantedName] = point;
+        pointsDict[point] = wantedName;
         point.name = "Point " + wantedName;
         point.transform.parent = pointsParent.transform;
         pointNameInputField.text = "";
@@ -80,14 +152,31 @@ public class InputPointCoords : MonoBehaviour
         }
     }
 
+    void TryToCreateLineSegment ()
+    {
+        if (createLineSegmentMode != 0)
+        {
+            firstLineSegmentSegment = null;
+            createLineSegmentMode = 0;
+            return;
+        }
+        if (pointsDict.Count < 2)
+        {
+            UnityEngine.Debug.Log("Need 2+ points to make a line segment.");
+            return;
+        }
+        UnityEngine.Debug.Log("Select first point.");
+        createLineSegmentMode = 1;
+    }
+
     void Start()
     {
-        Button btn = createPointButton.GetComponent<Button>();
-        btn.onClick.AddListener(TryToInstantiatePoint);
+        createPointButton.GetComponent<Button>().onClick.AddListener(TryToInstantiatePoint);
+        createLineSegmentButton.GetComponent<Button>().onClick.AddListener(TryToCreateLineSegment);
     }
 
     void Update()
     {
-        
+        HandleMouse(Input.GetMouseButtonUp(0));   
     }
 }
